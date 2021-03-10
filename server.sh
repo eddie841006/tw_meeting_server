5e2eda453cdae8a8afdd99fb658c8918cf440b46
eddie <s1074608@mail.yzu.edu.tw>
2021-03-10 10:35:25 +0800

    "test"

---
+++ server.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 /speaker/speaker_server.py
---
+++ test_server.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  4 11:03:12 2020
+
+@author: c95csy
+"""
+
+import requests
+import json
+
+ 
+url = "http://172.16.120.124:1111/"
+name = "subtitles_with_ASR_speaker2"
+data = {"video_name": name}
+resp = requests.post(url + "video_json", json=data)
+resp_data = json.loads(resp.text)
+print(resp_data)
